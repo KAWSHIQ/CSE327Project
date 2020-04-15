2f5e941c55b0c9c1881689d0321909a306f5f19a
Sudman1510794 <61133786+Sudman1510794@users.noreply.github.com>
2020-04-15 22:15:14 +0600

    Update connect.php

--- ProjectCompiler/Students/config/connect.php
+++ ProjectCompiler/Students/config/connect.php
@@ -1,1 +1,16 @@
-gjghjgjhjhg
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "project";
+
+$conn = new mysqli($servername, $username, $password,$db_name);
+
+if ($conn->connect_error) {
+  
+    die("Connection failed: " . $conn->connect_error);
+    
+} 
+
+?>
